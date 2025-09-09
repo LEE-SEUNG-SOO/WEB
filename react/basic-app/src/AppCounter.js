fcc62c139a77d3825751b5fa8f5490eac272b812
LEE <lsslcs01@gmail.com>
2025-09-09 18:09:38 +0900

    commit 20250908 LEE

--- react/basic-app/src/AppCounter.js
+++ react/basic-app/src/AppCounter.js
@@ -3,19 +3,18 @@ import { useState } from "react";
 import './App.css';
 
 // 자식 > 부모(누적합) > 자식(결과)
-
 export function App(){
     const [total, setTotal] = useState(0);
 
     const click = (number) =>{ // 자식 컴포넌트의 클릭이벤트 결과 가져오기
         setTotal(total + number); // 1씩 증감
     }
-
+// useState의 set함수를 파라미터로 연계해도 가능 total의 값을 실시간 변경 가능.
     return(
         <>
             <h1>Counter Test</h1>
-            <Counter click={click} total={total}/>
-            <Counter click={click} total={total}/>
+            <Counter click={click} total={total} setTotal={setTotal}/>
+            <Counter click={click} total={total} setTotal={setTotal}/>
         </>
     );
 }
--- react/basic-app/src/component/Counter.jsx
+++ react/basic-app/src/component/Counter.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-export function Counter({click, total}){
+export function Counter({click, total, setTotal}){
     const [number, setNumber] = useState(0); // 상태 변경시 변수값 변경설정
 
     const handleClickIncrement = () => {
        if(number < 10 ) {
             setNumber(number+1);
-            click(1);
+            // click(1); // 부모의 click메소드실행으로 total값 변경
+            setTotal(total+1); // 연계받은 setTotal의 함수를 자식이 사용해서 total값 변경
        }else {
            alert(`10이상증가불가`);
        }
@@ -14,14 +15,16 @@ export function Counter({click, total}){
     const handleClickDecrement = () => {
         if(number > 0) {
             setNumber(number-1);
-            click(-1);
+            // click(-1);
+            setTotal(total-1);
         } else {
             alert(`0이하감소불가`);
         }
     }
     const handleClickReset = () => {
         setNumber(0);
-        click(-number);
+        // click(-number);
+        setTotal(total-number);
     }
 
     return (
